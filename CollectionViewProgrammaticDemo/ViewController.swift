cc7563b3ecf94f823d86d3ab68f6008693bf1fb7
Matthew Dolan <mdolancode@gmail.com>
2024-11-22 10:31:46 -0800

    Set up simple and basic collection view.

--- CollectionViewProgrammaticDemo/ViewController.swift
+++ CollectionViewProgrammaticDemo/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    let data = Array(1...20)
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        return UICollectionView(frame: .zero, collectionViewLayout: layout)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        // Add collectionView to the view hierarchy
+        view.addSubview(collectionView)
+        
+        // Add collectionView constraints
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        // Set the delegate and dataSource
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        // Register a cell class
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemBlue
+        
+        // Add a label to display data
+        let label = UILabel(frame: cell.contentView.frame)
+        label.text = "\(data[indexPath.row])"
+        label.textAlignment = .center
+        label.textColor = .white
+        cell.contentView.addSubview(label)
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.width - 30) / 2
+        return CGSize(width: width, height: width)
+    }
 }
 
